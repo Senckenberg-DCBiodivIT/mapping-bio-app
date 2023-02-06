25b2c8c4b789e3b636cb905df2d44de7444e30b8
Alexander Wolodkin <alexander.wolodkin@senckenberg.com>
2023-02-06 11:08:47 +0100

    Add: query mappingRow

--- src/components/query.js
+++ src/components/query.js
@@ -25,4 +25,24 @@ export const query = {
         #BIND(IF(?hasSubClasses == true, ?anysubclass) as ?aSubclassID, ?aSubclassLabel )
     } 
   `,
+
+  mappingRow: `
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix owl: <http://www.w3.org/2002/07/owl#>
+
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+prefix align: <http://knowledgeweb.semanticweb.org/heterogeneity/alignment#>
+
+SELECT ?entity1 ?entity2 ?relation ?measure
+WHERE {
+  ?subject a align:Cell .
+  
+  ?subject align:entity1 ?entity1 .
+  ?subject align:entity2 ?entity2 .
+  
+  ?subject align:relation ?relation .
+  ?subject align:measure ?measure .
+}
+
+  `,
 };
