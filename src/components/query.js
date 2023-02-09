9694c4214ecd817681b1bbecce7047d71ef320e5
Alexander Wolodkin <alexander.wolodkin@senckenberg.com>
2023-02-09 15:51:54 +0100

    Add: getName

--- src/components/query.js
+++ src/components/query.js
@@ -45,4 +45,18 @@ WHERE {
 }
 
   `,
+
+  getName: `
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix owl: <http://www.w3.org/2002/07/owl#>
+
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+prefix align: <http://knowledgeweb.semanticweb.org/heterogeneity/alignment#>
+
+SELECT ?name 
+WHERE {
+  <ID_HERE> rdfs:label ?name .
+
+}
+  `,
 };
