e131fbbb099dd0a3c76170c2b5ed44362a73ae22
Alexander Wolodkin <alexander.wolodkin@senckenberg.de>
2023-08-30 09:24:16 +0200

    Add: Component files

---
+++ src/components/TheMappingtable.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "TheMappingtable",
+  props: [],
+  emit: [],
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/components/TreeStructure.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "TreeStructure",
+  props: [],
+  emit: [],
+  data() {
+    return {};
+  },
+};
+</script>
