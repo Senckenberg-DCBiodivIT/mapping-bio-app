9d5fad5e2cdc9b40fbc379d28c280f7978ebc57f
Alexander Wolodkin <alexander.wolodkin@senckenberg.de>
2023-09-05 15:55:35 +0200

    Adjust: component renamed

---
+++ src/components/TheTreeStructure.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <TreeView :kind="source" />
+    <TreeView :kind="target" />
+  </div>
+</template>
+
+<script setup>
+import TreeView from "@/components/TreeView";
+</script>
+
+<script>
+export default {
+  name: "TheTreeStructure",
+
+  data() {
+    return {};
+  },
+};
+</script>
