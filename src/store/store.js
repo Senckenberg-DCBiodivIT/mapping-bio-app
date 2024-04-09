37f69137f5825c29e8bb1f0a8ad5daf838a4c8d2
Alexander Wolodkin <alexander.wolodkin@senckenberg.de>
2024-04-09 15:34:29 +0200

    Add: Keycloak store

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,13 @@ import { createStore } from "vuex";
 
 import messengerMod from "./messenger";
 import mappingtableMod from "./mappingtable";
+import keycloakMod from "./keycloak";
 
 export default createStore({
   modules: {
     messenger: messengerMod,
     mappingtable: mappingtableMod,
+
+    keycloak: keycloakMod,
   },
 });
